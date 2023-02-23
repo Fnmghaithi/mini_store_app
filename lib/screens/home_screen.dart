db2939eb9fcee3e159bf8a92a3d3176fb5f5942e
Faisal <fnmghaithi@gmail.com>
2023-02-23 17:50:53 +0400

    Implemented the Swiper Widget.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:store_api_flutter_course/consts/global_colors.dart';
@@ -28,6 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
@@ -77,7 +79,22 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ),
-              SaleWidget(),
+              SizedBox(
+                height: size.height * 0.25,
+                child: Swiper(
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return const SaleWidget();
+                  },
+                  pagination: const SwiperPagination(
+                    alignment: Alignment.bottomCenter,
+                    builder: DotSwiperPaginationBuilder(
+                      activeColor: Colors.red,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
