dd4be6795edb789514385494fa4d1923f300c82f
Faisal <fnmghaithi@gmail.com>
2023-02-24 18:10:40 +0400

    implemented the product details screen.

---
+++ lib/screens/product_details_screen.dart
@@ -0,0 +1,123 @@
+import 'package:card_swiper/card_swiper.dart';
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:flutter/material.dart';
+import 'package:store_api_flutter_course/consts/global_colors.dart';
+
+class ProductDetails extends StatefulWidget {
+  const ProductDetails({super.key});
+
+  @override
+  State<ProductDetails> createState() => _ProductDetailsState();
+}
+
+class _ProductDetailsState extends State<ProductDetails> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Category',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const SizedBox(height: 18),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Flexible(
+                      flex: 2,
+                      child: Text(
+                        'Lorem Ipsum',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: RichText(
+                        text: TextSpan(
+                          text: '\$',
+                          style: const TextStyle(
+                            fontSize: 25,
+                            color: Color.fromRGBO(33, 150, 243, 1),
+                          ),
+                          children: [
+                            TextSpan(
+                              text: '168.00',
+                              style: TextStyle(
+                                color: lightTextColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: size.height * 0.4,
+                  child: Swiper(
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return FancyShimmerImage(
+                        width: double.infinity,
+                        boxFit: BoxFit.fill,
+                        imageUrl: 'https://i.ibb.co/vwB46Yq/shoes.png',
+                      );
+                    },
+                    autoplay: true,
+                    pagination: const SwiperPagination(
+                      alignment: Alignment.bottomCenter,
+                      builder: DotSwiperPaginationBuilder(
+                        activeColor: Colors.red,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 18),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Description',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 18),
+                      Text(
+                        'Lorem Ipsum is simply dummy text of the printing and type and typesetting industry. Lorem Ipsum is simply dummy text of the printing and type and typesetting industry. Lorem Ipsum is simply dummy text of the printing and type and typesetting industry.',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
