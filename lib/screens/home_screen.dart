c15f10a0ee5c7d135ad96ba3cd496b4e691592dc
Faisal <fnmghaithi@gmail.com>
2023-02-24 17:01:36 +0400

    Implemented the All Products Screen.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:store_api_flutter_course/consts/global_colors.dart';
+import 'package:store_api_flutter_course/screens/feeds_screen.dart';
 import 'package:store_api_flutter_course/widgets/appbar_icons.dart';
 import 'package:store_api_flutter_course/widgets/feeds_widget.dart';
 import 'package:store_api_flutter_course/widgets/sale_widget.dart';
@@ -113,7 +115,15 @@ class _HomeScreenState extends State<HomeScreen> {
                             ),
                             const Spacer(),
                             AppBarIcons(
-                              function: () {},
+                              function: () {
+                                Navigator.push(
+                                  context,
+                                  PageTransition(
+                                    child: const FeedsScreen(),
+                                    type: PageTransitionType.fade,
+                                  ),
+                                );
+                              },
                               icon: IconlyBold.arrowRight2,
                             )
                           ],
